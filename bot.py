bb86099e457eb05ade66c31f339a3a7101cba188
alexbagirov <a.d.bagirov@gmail.com>
2017-08-29 02:04:48 +0500

    :art: Switches bot to use Postgres, improves code quality
    Former-commit-id: e59d1ae68630fb5d2158cca0c313f2a02d6961ca

--- bot.py
+++ bot.py
@@ -1,116 +1,123 @@
-import logging, os, re, requests, sqlite3, telebot, time
-from bs4 import BeautifulSoup
+import logging, requests, psycopg2, telebot
 from telebot import types
-from selenium import webdriver
-from PIL import Image
+from phrases import *
+from bs4 import BeautifulSoup
 
 bot = telebot.TeleBot('405295345:AAEiq-A3mEVsE203a0qOM3z2QCpPOlMKbZ0')
 logger = telebot.logger
-telebot.logger.setLevel(logging.ERROR)
+telebot.logger.setLevel(logging.INFO)
+db_creds = {'dbname': 'bot',
+            'user': 'ubuntu',
+            'password': 'sas'}
 
 nmaps_chat = '-1001136617457'
 mods_chat = '-240980847'
 roads_chat = '-227479062'
 
-nmaps_link = re.compile('https:\/\/n.maps.yandex[a-zA-Z./#!?=&0-9%]+')
-nmaps_short_link = re.compile('https:\/\/n.maps.yandex.[a-z]+\/-\/[A-Za-z0-9-]+')
-maps_link = re.compile('https:\/\/yandex.[a-z]+\/maps\/[0-9a-zA-Z/?=.%&-]*')
+kbrd_btn = types.InlineKeyboardButton
 
-db = sqlite3.connect('database.db')
+db = psycopg2.connect(**db_creds)
 c = db.cursor()
-c.execute('CREATE TABLE IF NOT EXISTS roads (username text, chat_message_id text, mods_message_id text, roads_message_id text)')
+c.execute('''CREATE TABLE IF NOT EXISTS roads (username text,
+                                               chat_message_id bigint,
+                                               mods_message_id bigint,
+                                               roads_message_id bigint)''')
 c.execute('CREATE TABLE IF NOT EXISTS banned (username text)')
-c.execute('CREATE TABLE IF NOT EXISTS admins (username text)')
-c.execute('INSERT INTO admins VALUES ("alexfox")')
 db.commit()
-c.close()
 db.close()
 
 
 def private_chat(message):
-    if message.chat.type == 'private':
-        return True
-
-
-def from_admin(message):
-    db = sqlite3.connect('database.db')
-    c = db.cursor()
-    c.execute('SELECT username FROM admins WHERE username = ?', [str(message.from_user.username)])
-    if c.fetchall():
-        return True
+    return message.chat.type == 'private'
 
 
 @bot.message_handler(commands=['start', 'home'])
 def home(message):
     if not private_chat(message):
         return
-    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
-    keyboard.row('📌 Полезные ссылки')
-    keyboard.row('🔎 Поиск в Клубе', '🔎 Поиск в Правилах')
-    if from_admin(message):
-        keyboard.row('🚫 Сообщить о перекрытии')
-        keyboard.row('📚 Частые вопросы', '✏ Служба поддержки')
-        keyboard.row('Чёрный список')
-    bot.send_message(message.chat.id, 'Пожалуйста, выберите действие.', reply_markup=keyboard)
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True,
+               one_time_keyboard=True)
+    keyboard.row(MENU_LINKS)
+    keyboard.row(MENU_SEARCH_CLUB, MENU_SEARCH_RULES)
+    keyboard.row(MENU_ROADS)
+    keyboard.row(MENU_FAQ, MENU_SUPPORT)
+    bot.send_message(message.chat.id,
+                     BOT_ACTION_SELECT,
+                     reply_markup=keyboard)
 
-
-@bot.message_handler(regexp='📌 Полезные ссылки')
+@bot.message_handler(regexp=MENU_LINKS)
 def bookmarks(message):
     if not private_chat(message):
         return
     keyboard = types.InlineKeyboardMarkup()
-    url1 = types.InlineKeyboardButton(text='Правила', url='https://yandex.ru/support/nmaps/rules_2.html')
-    url2 = types.InlineKeyboardButton(text='Клуб', url='https://yandex.ru/blog/narod-karta')
-    url3 = types.InlineKeyboardButton(text='ПКК', url='https://pkk5.rosreestr.ru/')
-    url4 = types.InlineKeyboardButton(text='ФИАС', url='https://fias.nalog.ru/')
-    url5 = types.InlineKeyboardButton(text='ЕГРП365', url='https://egrp365.ru/map/')
-    url6 = types.InlineKeyboardButton(text='TerraServer', url='https://www.terraserver.com/')
-    url7 = types.InlineKeyboardButton(text='Реформа ЖКХ', url='https://www.reformagkh.ru/')
+
+    url1 = kbrd_btn(text='Правила',
+                    url='https://yandex.ru/support/nmaps/rules_2.html')
+    url2 = kbrd_btn(text='Клуб',
+                    url='https://yandex.ru/blog/narod-karta')
+    url3 = kbrd_btn(text='ПКК',
+                    url='https://pkk5.rosreestr.ru/')
+    url4 = kbrd_btn(text='ФИАС',
+                    url='https://fias.nalog.ru/')
+    url5 = kbrd_btn(text='ЕГРП365',
+                    url='https://egrp365.ru/map/')
+    url6 = kbrd_btn(text='TerraServer',
+                    url='https://www.terraserver.com/')
+    url7 = kbrd_btn(text='Реформа ЖКХ',
+                    url='https://www.reformagkh.ru/')
     keyboard.add(url1, url2, url3, url4, url5, url6, url7)
-    bot.send_message(message.chat.id, 'Для перехода на сайт нажмите нужную кнопку из списка.', reply_markup=keyboard)
+    bot.send_message(message.chat.id,
+                     BOT_CHS_LINK,
+                     reply_markup=keyboard)
     home(message)
 
 
-@bot.message_handler(regexp='🔎 Поиск')
+@bot.message_handler(regexp=MENU_SEARCH)
 def search(message):
     if not private_chat(message):
         return
-    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
-    keyboard.row('⬅ Вернуться')
-    bot.send_message(message.chat.id, 'Напишите фразу для поиска.', reply_markup=keyboard)
-    if message.text == '🔎 Поиск в Правилах':
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True,
+                                         one_time_keyboard=True)
+    keyboard.row(MENU_RETURN)
+    bot.send_message(message.chat.id,
+                     BOT_SRCH_QUERY,
+                     reply_markup=keyboard)
+    if message.text == MENU_SEARCH_RULES:
         bot.register_next_step_handler(message, search_rules)
-    elif message.text == '🔎 Поиск в Клубе':
+    elif message.text == MENU_SEARCH_CLUB:
         bot.register_next_step_handler(message, search_club)
-    elif message.text == '⬅ Вернуться':
+    elif message.text == MENU_RETURN:
         home(message)
 
 
 def search_club(message):
-    if message.text == '⬅ Вернуться':
+    if message.text == MENU_RETURN:
         home(message)
         return
-    page = requests.get('https://yandex.ru/blog/narod-karta/search?text=' + message.text.replace(' ', '+'))
+    page = requests.get('https://yandex.ru/blog/narod-karta/search?text=' +
+                        message.text.replace(' ', '+'))
     soup = BeautifulSoup(page.text, 'lxml')
     answer = ''
     for item in soup.find_all('a', class_='b-serp-item'):
-        pass
         title = item.find('h2').text
         link = 'https://yandex.ru' + item['href']
         answer += '[' + title + '](' + link + ')\n'
         answer += '____________________\n'
     if not answer:
-        bot.send_message(message.chat.id, 'К сожалению, ничего не найдено.')
+        bot.send_message(message.chat.id, BOT_NOT_FOUND)
     else:
-        bot.send_message(message.chat.id, answer, parse_mode='markdown', disable_web_page_preview=True)
+        bot.send_message(message.chat.id, answer, parse_mode='markdown',
+                         disable_web_page_preview=True)
     home(message)
 
 
 def search_rules(message):
-    if message.text == '⬅ Вернуться':
+    if message.text == MENU_RETURN:
         home(message)
         return
-    page = requests.get('https://yandex.ru/support/search-results/?text=' + message.text.replace(' ', '+') + '&service=nmaps-guide')
+    page = requests.get('https://yandex.ru/support/search-results/?text=' +
+                        message.text.replace(' ', '+') +
+                        '&service=nmaps-guide')
     soup = BeautifulSoup(page.text, 'lxml')
     answer = ''
     for item in soup.find_all('a', class_='serp__item'):
@@ -123,105 +130,89 @@ def search_rules(message):
         answer += '[' + title + '](' + link + '): ' + excerpt + '\n'
         answer += '____________________\n'
     if not answer:
-        bot.send_message(message.chat.id, 'К сожалению, ничего не найдено.')
+        bot.send_message(message.chat.id, BOT_NOT_FOUND)
     else:
-        bot.send_message(message.chat.id, answer, parse_mode='markdown', disable_web_page_preview=True)
+        bot.send_message(message.chat.id, answer, parse_mode='markdown',
+                         disable_web_page_preview=True)
     home(message)
 
 
 @bot.message_handler(content_types=['text'])
 def roads(message):
-    links = re.findall(nmaps_link, message.text) + re.findall(nmaps_short_link, message.text) + re.findall(maps_link, message.text)
     if '#перекрытие' in message.text:
-        return
-        db = sqlite3.connect('database.db')
-        c = db.cursor()
-        c.execute('SELECT username FROM banned WHERE username = ?', [str(message.from_user.username)])
-        if c.fetchall():
-            bot.reply_to(message, 'Вы были внесены в черный список и не можете передавать сообщения.')
-            return
-
-        bot.send_message(message.chat.id, '@' + message.from_user.username + ', сообщение принято. Спасибо!')
+        bot.send_message(message.chat.id,
+                         BOT_MSG_ACCEPT.format(message.from_user.username))
 
         keyboard = types.InlineKeyboardMarkup()
-        keyboard.row(types.InlineKeyboardButton(text='↩ Передать сотрудникам', callback_data='road_mod_approve'))
-        keyboard.row(types.InlineKeyboardButton(text='🔫 Запросить информацию', callback_data='road_mod_request_info'))
-        keyboard.row(types.InlineKeyboardButton(text='🚫 Вандализм', callback_data='road_mod_ban'))
-        mods_message = bot.send_message(mods_chat, 'Пользователь @' + message.from_user.username + ' оставил следующее сообщение. Просьба проверить информацию.', reply_markup=keyboard)
+        keyboard.row(kbrd_btn(text=BTN_ROADS_ACCEPT,
+                              callback_data='road_mod_approve'))
+        keyboard.row(kbrd_btn(text=BTN_ROADS_REQUEST_INFO,
+                              callback_data='road_mod_request_info'))
+        keyboard.row(kbrd_btn(text=BTN_ROADS_FRAUD,
+                              callback_data='road_mod_ban'))
+        mods_message = bot.send_message(mods_chat,
+                                        BOT_REQUEST_CHECK.format(message.from_user.username), reply_markup=keyboard)
         bot.forward_message(mods_chat, nmaps_chat, message.message_id)
-        c.execute('INSERT INTO roads VALUES (?, ?, ?, ?)', [str(message.from_user.username), str(message.message_id), str(mods_message.message_id), str(0)])
+        db = psycopg2.connect(**db_creds)
+        c = db.cursor()
+        c.execute('INSERT INTO roads VALUES (%s, %s, %s, %s)',
+                  (message.from_user.username, message.message_id,
+                   mods_message.message_id, 0))
         db.commit()
         db.close()
-    elif links:
-        bot.send_chat_action(message.chat.id, 'upload_photo')
-        chrome = 'Mozilla/5.0 (X11; Linux x86_64; rv:10.0) Gecko/20100101 Firefox/54.0'
-        webdriver.DesiredCapabilities.PHANTOMJS['phantomjs.page.customHeaders.User-Agent'] = chrome
-        driver = webdriver.PhantomJS('./phantomjs')
-        driver.set_window_size(1280, 1024)
-
-        for link in links:
-            driver.get(link)
-            time.sleep(10)
-            bot.send_chat_action(message.chat.id, 'upload_photo')
-            if 'n.' in link:
-                driver.find_element_by_xpath('//div[@class="nk-welcome-screen-view__start"]/button').click()
-            image_name = str(time.time())
-            driver.save_screenshot(image_name + '.png')
-            if '#!/?z=' in driver.current_url:
-                i = Image.open(image_name + '.png')
-                i.crop((0, 0, 930, 1024)).save(image_name, 'PNG')
-                bot.send_photo(message.chat.id, open(image_name, 'rb'))
-            else:
-                bot.send_photo(message.chat.id, open(image_name + '.png', 'rb'))
-            try:
-                os.remove('.' + image_name + '.png')
-            except Exception:
-                pass
 
 
 @bot.callback_query_handler(func=lambda call: True)
-def callback(call):
-    db = sqlite3.connect('database.db')
+def roads_callback(call):
+    db = psycopg2.connect(**db_creds)
     c = db.cursor()
 
     if call.data == 'road_mod_approve':
-        bot.edit_message_text('✅ Направлено сотрудникам', chat_id=call.message.chat.id, message_id=call.message.message_id)
+        bot.edit_message_text(BOT_SENT_TO_STAFF,
+                              chat_id=call.message.chat.id,
+                              message_id=call.message.message_id)
 
         keyboard = types.InlineKeyboardMarkup()
-        keyboard.row(types.InlineKeyboardButton(text='🚧 Перекрытие установлено', callback_data='road_closed'))
-        keyboard.row(types.InlineKeyboardButton(text='🚗 Перекрытие снято', callback_data='road_opened'))
-        keyboard.row(types.InlineKeyboardButton(text='⚠ Инфоточка установлена', callback_data='info_added'))
-        keyboard.row(types.InlineKeyboardButton(text='🔫 Запросить информацию', callback_data='road_request_info'))
-        roads_message = bot.send_message(roads_chat, 'Добрый день. Появилась новая информация о перекрытии.', reply_markup=keyboard)
+        keyboard.row(kbrd_btn(text=BTN_ROADS_CLOSED,
+                              callback_data='road_closed'))
+        keyboard.row(kbrd_btn(text=BTN_ROADS_OPENED,
+                              callback_data='road_opened'))
+        keyboard.row(kbrd_btn(text=BTN_ROADS_INFOPOINT,
+                              callback_data='info_added'))
+        keyboard.row(kbrd_btn(text=BTN_ROADS_REQUEST_INFO,
+                              callback_data='road_request_info'))
+        roads_message = bot.send_message(roads_chat,
+                                         BOT_NEW_ROADBLOCK,
+                                         reply_markup=keyboard)
 
-        c.execute('SELECT chat_message_id FROM roads WHERE mods_message_id = ?', [str(call.message.message_id)])
+        c.execute('SELECT chat_message_id FROM roads WHERE mods_message_id = %s', (call.message.message_id,))
         chat_message_id = c.fetchall()[0][0]
         bot.forward_message(roads_chat, nmaps_chat, chat_message_id)
 
-        c.execute('UPDATE roads SET roads_message_id = ? WHERE chat_message_id = ?', [str(roads_message.message_id), str(chat_message_id)])
+        c.execute('UPDATE roads SET roads_message_id = %s WHERE chat_message_id = %s', (roads_message.message_id, chat_message_id,))
     elif call.data == 'road_mod_request_info':
-        bot.edit_message_text('📋 Ожидается информация', chat_id=call.message.chat.id, message_id=call.message.message_id)
+        bot.edit_message_text(BOT_WAIT_FOR_INFO, chat_id=call.message.chat.id, message_id=call.message.message_id)
     elif call.data == 'road_mod_ban':
-        bot.edit_message_text('🚫 Пользователь заблокирован', chat_id=call.message.chat.id, message_id=call.message.message_id)
-        c.execute('INSERT INTO banned SELECT username FROM roads WHERE mods_message_id = ?', [str(call.message.message_id)])
+        bot.edit_message_text(BOT_USER_BANNED, chat_id=call.message.chat.id, message_id=call.message.message_id)
+        c.execute('INSERT INTO banned VALUES (SELECT username FROM roads WHERE mods_message_id = %s)', (call.message.message_id,))
     elif call.data == 'road_closed':
-        bot.edit_message_text('✅ Перекрытие установлено', chat_id=call.message.chat.id, message_id=call.message.message_id)
-        c.execute('SELECT chat_message_id FROM roads WHERE roads_message_id = ?', [call.message.message_id])
+        bot.edit_message_text(BOT_ROADBLOCK_SET, chat_id=call.message.chat.id, message_id=call.message.message_id)
+        c.execute('SELECT chat_message_id FROM roads WHERE roads_message_id = %s', (call.message.message_id,))
         chat_message_id = c.fetchall()
-        bot.send_message(nmaps_chat, 'Перекрытие установлено, спасибо!', reply_to_message_id=chat_message_id)
+        bot.send_message(nmaps_chat, BOT_ROADBLOCK_SET_USR, reply_to_message_id=chat_message_id)
     elif call.data == 'road_opened':
-        bot.edit_message_text('✅ Перекрытие снято', chat_id=call.message.chat.id, message_id=call.message.message_id)
-        c.execute('SELECT chat_message_id FROM roads WHERE roads_message_id = ?', [call.message.message_id])
+        bot.edit_message_text(BOT_ROADBLOCK_DEL, chat_id=call.message.chat.id, message_id=call.message.message_id)
+        c.execute('SELECT chat_message_id FROM roads WHERE roads_message_id = %s', (call.message.message_id,))
         chat_message_id = c.fetchall()
-        bot.send_message(nmaps_chat, 'Перекрытие снято, спасибо!', reply_to_message_id=chat_message_id)
+        bot.send_message(nmaps_chat, BOT_ROADBLOCK_DEL_USR, reply_to_message_id=chat_message_id)
     elif call.data == 'info_added':
-        bot.edit_message_text('✅ Инфоточка установлена', chat_id=call.message.chat.id, message_id=call.message.message_id)
-        c.execute('SELECT chat_message_id FROM roads WHERE roads_message_id = ?', [call.message.message_id])
+        bot.edit_message_text(BOT_INFOPOINT_SET, chat_id=call.message.chat.id, message_id=call.message.message_id)
+        c.execute('SELECT chat_message_id FROM roads WHERE roads_message_id = %s', (call.message.message_id,))
         chat_message_id = c.fetchall()
-        bot.send_message(nmaps_chat, 'Инфоточка установлена, спасибо!', reply_to_message_id=chat_message_id)
+        bot.send_message(nmaps_chat, BOT_INFOPOINT_SET_USR, reply_to_message_id=chat_message_id)
     elif call.data == 'road_request_info':
-        bot.edit_message_text('📋 Ожидается информация', chat_id=call.message.chat.id, message_id=call.message.message_id)
-        c.execute('INSERT INTO banned SELECT username FROM roads WHERE mods_message_id = ?', [str(call.message.message_id)])
+        bot.edit_message_text(BOT_WAIT_FOR_INFO, chat_id=call.message.chat.id, message_id=call.message.message_id)
+        c.execute('INSERT INTO banned SELECT username FROM roads WHERE mods_message_id = %s', (call.message.message_id,))
 
     db.commit()
     db.close()
