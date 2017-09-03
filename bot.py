b3d83cf81034ed17fb4f6fbf211c277e5b70c441
alexbagirov <a.d.bagirov@gmail.com>
2017-09-04 01:19:43 +0500

    :sparkles: Finishes roads features, adds admins and settings
    Former-commit-id: 26f7241ae3b4cd75bab4dfe30544a3b14ca62023

--- bot.py
+++ bot.py
@@ -3,7 +3,17 @@ from telebot import types
 from phrases import *
 from bs4 import BeautifulSoup
 
+
+class Prefix:
+    def __init__(self, prefix):
+        self.prefix = prefix
+
+    def __call__(self, call):
+        return call.data.startswith(self.prefix)
+
+
 bot = telebot.TeleBot('405295345:AAEiq-A3mEVsE203a0qOM3z2QCpPOlMKbZ0')
+bot.bypass_moderators = False
 logger = telebot.logger
 telebot.logger.setLevel(logging.INFO)
 db_creds = {'dbname': 'bot',
@@ -22,7 +32,11 @@ c.execute('''CREATE TABLE IF NOT EXISTS roads (username text,
                                                chat_message_id bigint,
                                                mods_message_id bigint,
                                                roads_message_id bigint)''')
-c.execute('CREATE TABLE IF NOT EXISTS banned (username text)')
+c.execute('''CREATE TABLE IF NOT EXISTS banned (username text primary key)''')
+c.execute('''CREATE TABLE IF NOT EXISTS admins (username text primary key)''')
+c.execute('''DELETE FROM admins''')
+c.execute('''INSERT INTO admins VALUES ('leva18777')''')
+c.execute('''INSERT INTO admins VALUES ('alexfox')''')
 db.commit()
 db.close()
 
@@ -31,19 +45,51 @@ def private_chat(message):
     return message.chat.type == 'private'
 
 
+def is_admin(user):
+    db = psycopg2.connect(**db_creds)
+    c = db.cursor()
+    c.execute('''SELECT username FROM admins
+                 WHERE username = %s''', (user,))
+    is_adm = c.fetchone() is not None
+    db.close()
+    return is_adm
+
+
 @bot.message_handler(commands=['start', 'home'])
 def home(message):
     if not private_chat(message):
         return
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True,
-               one_time_keyboard=True)
+                                         one_time_keyboard=True)
     keyboard.row(MENU_LINKS)
     keyboard.row(MENU_SEARCH_CLUB, MENU_SEARCH_RULES)
     keyboard.row(MENU_ROADS)
     keyboard.row(MENU_FAQ, MENU_SUPPORT)
+    if is_admin(message.from_user.username):
+        keyboard.row(MENU_SETTINGS)
     bot.send_message(message.chat.id,
                      BOT_ACTION_SELECT,
                      reply_markup=keyboard)
+
+
+@bot.message_handler(regexp=MENU_SETTINGS)
+def settings(message):
+    keyboard = types.InlineKeyboardMarkup()
+    if bot.bypass_moderators:
+        keyboard.row(kbrd_btn(text=PREF_MOD_ON,
+                              callback_data='prefs_mod_on'))
+    else:
+        keyboard.row(kbrd_btn(text=PREF_MOD_OFF,
+                              callback_data='prefs_mod_off'))
+    keyboard.row(kbrd_btn(text=PREF_ADMINS,
+                          callback_data='prefs_admins'))
+    keyboard.row(kbrd_btn(text=MENU_RETURN,
+                          callback_data='prefs_return'))
+
+    bot.send_message(message.chat.id,
+                     PREF_DESC,
+                     reply_markup=keyboard)
+
 
 @bot.message_handler(regexp=MENU_LINKS)
 def bookmarks(message):
@@ -137,12 +183,61 @@ def search_rules(message):
     home(message)
 
 
+def add_admin(message):
+    db = psycopg2.connect(**db_creds)
+    c = db.cursor()
+    try:
+        c.execute('INSERT INTO admins VALUES (%s)', (message.text.strip('@'), ))
+    except psycopg2.IntegrityError:
+        bot.send_message(message.chat.id, PREF_ALREADY_ADMIN_USR)
+        home(message)
+        return
+    db.commit()
+    c.close()
+    db.close()
+    bot.send_message(message.chat.id, PREF_ADDED_ADMIN_USR)
+    home(message)
+
+
+def del_admin(message):
+    db = psycopg2.connect(**db_creds)
+    c = db.cursor()
+    c.execute('DELETE FROM admins WHERE username = %s', (message.text.strip('@'), ))
+    db.commit()
+    c.close()
+    db.close()
+    bot.send_message(message.chat.id, PREF_DELED_ADMIN_USR)
+    home(message)
+
+
 @bot.message_handler(content_types=['text'])
 def roads(message):
-    if '#перекрытие' in message.text:
-        bot.send_message(message.chat.id,
-                         BOT_MSG_ACCEPT.format(message.from_user.username))
+    if '#перекрытие' not in message.text.lower():
+        return
 
+    bot.send_message(message.chat.id,
+                     BOT_MSG_ACCEPT.format(message.from_user.username))
+
+    db = psycopg2.connect(**db_creds)
+    c = db.cursor()
+
+    if bot.bypass_moderators:
+        keyboard = types.InlineKeyboardMarkup()
+        keyboard.row(kbrd_btn(text=BTN_ROADS_CLOSED,
+                              callback_data='road_closed'))
+        keyboard.row(kbrd_btn(text=BTN_ROADS_OPENED,
+                              callback_data='road_opened'))
+        keyboard.row(kbrd_btn(text=BTN_ROADS_INFOPOINT,
+                              callback_data='road_info_added'))
+        roads_message = bot.send_message(roads_chat,
+                                         BOT_NEW_ROADBLOCK,
+                                         reply_markup=keyboard)
+
+        bot.forward_message(roads_chat, nmaps_chat, message.message_id)
+        c.execute('''INSERT INTO roads VALUES (%s, %s, %s, %s)''',
+                  (message.from_user.username, message.message_id,
+                   0, roads_message.message_id))
+    else:
         keyboard = types.InlineKeyboardMarkup()
         keyboard.row(kbrd_btn(text=BTN_ROADS_ACCEPT,
                               callback_data='road_mod_approve'))
@@ -150,25 +245,27 @@ def roads(message):
                               callback_data='road_mod_request_info'))
         keyboard.row(kbrd_btn(text=BTN_ROADS_FRAUD,
                               callback_data='road_mod_ban'))
-        mods_message = bot.send_message(mods_chat,
-                                        BOT_REQUEST_CHECK.format(message.from_user.username), reply_markup=keyboard)
+
+        msg = BOT_REQUEST_CHECK.format(message.from_user.username)
+        mods_message = bot.send_message(mods_chat, msg, reply_markup=keyboard)
         bot.forward_message(mods_chat, nmaps_chat, message.message_id)
-        db = psycopg2.connect(**db_creds)
-        c = db.cursor()
-        c.execute('INSERT INTO roads VALUES (%s, %s, %s, %s)',
+        c.execute('''INSERT INTO roads VALUES (%s, %s, %s, %s)''',
                   (message.from_user.username, message.message_id,
                    mods_message.message_id, 0))
-        db.commit()
-        db.close()
+    db.commit()
+    db.close()
 
 
-@bot.callback_query_handler(func=lambda call: True)
+@bot.callback_query_handler(func=Prefix('road_'))
 def roads_callback(call):
     db = psycopg2.connect(**db_creds)
     c = db.cursor()
 
     if call.data == 'road_mod_approve':
-        bot.edit_message_text(BOT_SENT_TO_STAFF,
+        if 'Выясняет' in call.message.text and call.from_user.username not in call.message.text:
+            bot.answer_callback_query(call.id, text=BOT_UNDER_INVESTIGATION)
+            return
+        bot.edit_message_text(BOT_SENT_TO_STAFF.format(call.from_user.username),
                               chat_id=call.message.chat.id,
                               message_id=call.message.message_id)
 
@@ -178,44 +275,128 @@ def roads_callback(call):
         keyboard.row(kbrd_btn(text=BTN_ROADS_OPENED,
                               callback_data='road_opened'))
         keyboard.row(kbrd_btn(text=BTN_ROADS_INFOPOINT,
-                              callback_data='info_added'))
-        keyboard.row(kbrd_btn(text=BTN_ROADS_REQUEST_INFO,
-                              callback_data='road_request_info'))
+                              callback_data='road_info_added'))
         roads_message = bot.send_message(roads_chat,
                                          BOT_NEW_ROADBLOCK,
                                          reply_markup=keyboard)
 
-        c.execute('SELECT chat_message_id FROM roads WHERE mods_message_id = %s', (call.message.message_id,))
-        chat_message_id = c.fetchall()[0][0]
+        c.execute('''SELECT chat_message_id FROM roads
+                     WHERE mods_message_id = %s''',
+                  (call.message.message_id,))
+        chat_message_id = c.fetchone()[0]
         bot.forward_message(roads_chat, nmaps_chat, chat_message_id)
 
-        c.execute('UPDATE roads SET roads_message_id = %s WHERE chat_message_id = %s', (roads_message.message_id, chat_message_id,))
+        c.execute('''UPDATE roads SET roads_message_id = %s
+                     WHERE chat_message_id = %s''',
+                  (roads_message.message_id, chat_message_id,))
     elif call.data == 'road_mod_request_info':
-        bot.edit_message_text(BOT_WAIT_FOR_INFO, chat_id=call.message.chat.id, message_id=call.message.message_id)
+        keyboard = types.InlineKeyboardMarkup()
+        keyboard.row(kbrd_btn(text=BTN_ROADS_ACCEPT,
+                              callback_data='road_mod_approve'))
+        msg = BOT_INVESTIGATING.format(call.from_user.username)
+        bot.edit_message_text(msg, chat_id=call.message.chat.id,
+                              message_id=call.message.message_id,
+                              reply_markup=keyboard)
     elif call.data == 'road_mod_ban':
-        bot.edit_message_text(BOT_USER_BANNED, chat_id=call.message.chat.id, message_id=call.message.message_id)
-        c.execute('INSERT INTO banned VALUES (SELECT username FROM roads WHERE mods_message_id = %s)', (call.message.message_id,))
+        bot.edit_message_text(BOT_USER_BANNED.format(call.from_user.username),
+                              chat_id=call.message.chat.id,
+                              message_id=call.message.message_id)
+        c.execute('''INSERT INTO banned VALUES
+                     (SELECT username FROM roads
+                      WHERE mods_message_id = %s)''',
+                  (call.message.message_id,))
     elif call.data == 'road_closed':
-        bot.edit_message_text(BOT_ROADBLOCK_SET, chat_id=call.message.chat.id, message_id=call.message.message_id)
-        c.execute('SELECT chat_message_id FROM roads WHERE roads_message_id = %s', (call.message.message_id,))
+        bot.edit_message_text(BOT_ROADBLOCK_SET, chat_id=call.message.chat.id,
+                              message_id=call.message.message_id)
+        c.execute('''SELECT chat_message_id FROM roads
+                     WHERE roads_message_id = %s''',
+                  (call.message.message_id,))
         chat_message_id = c.fetchall()
-        bot.send_message(nmaps_chat, BOT_ROADBLOCK_SET_USR, reply_to_message_id=chat_message_id)
+        bot.send_message(nmaps_chat, BOT_ROADBLOCK_SET_USR,
+                         reply_to_message_id=chat_message_id)
     elif call.data == 'road_opened':
-        bot.edit_message_text(BOT_ROADBLOCK_DEL, chat_id=call.message.chat.id, message_id=call.message.message_id)
-        c.execute('SELECT chat_message_id FROM roads WHERE roads_message_id = %s', (call.message.message_id,))
+        bot.edit_message_text(BOT_ROADBLOCK_DEL, chat_id=call.message.chat.id,
+                              message_id=call.message.message_id)
+        c.execute('''SELECT chat_message_id FROM roads
+                     WHERE roads_message_id = %s''',
+                  (call.message.message_id,))
         chat_message_id = c.fetchall()
-        bot.send_message(nmaps_chat, BOT_ROADBLOCK_DEL_USR, reply_to_message_id=chat_message_id)
-    elif call.data == 'info_added':
-        bot.edit_message_text(BOT_INFOPOINT_SET, chat_id=call.message.chat.id, message_id=call.message.message_id)
-        c.execute('SELECT chat_message_id FROM roads WHERE roads_message_id = %s', (call.message.message_id,))
+        bot.send_message(nmaps_chat, BOT_ROADBLOCK_DEL_USR,
+                         reply_to_message_id=chat_message_id)
+    elif call.data == 'road_info_added':
+        bot.edit_message_text(BOT_INFOPOINT_SET, chat_id=call.message.chat.id,
+                              message_id=call.message.message_id)
+        c.execute('''SELECT chat_message_id FROM roads
+                     WHERE roads_message_id = %s''',
+                  (call.message.message_id,))
         chat_message_id = c.fetchall()
-        bot.send_message(nmaps_chat, BOT_INFOPOINT_SET_USR, reply_to_message_id=chat_message_id)
-    elif call.data == 'road_request_info':
-        bot.edit_message_text(BOT_WAIT_FOR_INFO, chat_id=call.message.chat.id, message_id=call.message.message_id)
-        c.execute('INSERT INTO banned SELECT username FROM roads WHERE mods_message_id = %s', (call.message.message_id,))
+        bot.send_message(nmaps_chat, BOT_INFOPOINT_SET_USR,
+                         reply_to_message_id=chat_message_id)
 
     db.commit()
     db.close()
+
+
+@bot.callback_query_handler(func=Prefix('prefs_'))
+def prefs_callback(call):
+    keyboard = types.InlineKeyboardMarkup()
+
+    if call.data == 'prefs_mod_on':
+        bot.bypass_moderators = False
+        keyboard.row(kbrd_btn(text=PREF_MOD_OFF,
+                              callback_data='prefs_mod_off'))
+        keyboard.row(kbrd_btn(text=PREF_ADMINS,
+                              callback_data='prefs_admins'))
+        keyboard.row(kbrd_btn(text=MENU_RETURN,
+                              callback_data='prefs_return'))
+        bot.edit_message_text(PREF_DESC, chat_id=call.message.chat.id,
+                              message_id=call.message.message_id,
+                              reply_markup=keyboard)
+    elif call.data == 'prefs_mod_off':
+        bot.bypass_moderators = True
+        keyboard.row(kbrd_btn(text=PREF_MOD_ON,
+                              callback_data='prefs_mod_on'))
+        keyboard.row(kbrd_btn(text=PREF_ADMINS,
+                              callback_data='prefs_admins'))
+        keyboard.row(kbrd_btn(text=MENU_RETURN,
+                              callback_data='prefs_return'))
+        bot.edit_message_text(PREF_DESC, chat_id=call.message.chat.id,
+                              message_id=call.message.message_id,
+                              reply_markup=keyboard)
+    elif call.data == 'prefs_admins':
+        keyboard.row(kbrd_btn(text=PREF_ADD_ADMIN,
+                              callback_data='admin_add'))
+        keyboard.row(kbrd_btn(text=PREF_DEL_ADMIN,
+                              callback_data='admin_del'))
+        keyboard.row(kbrd_btn(text=PREF_LIST_ADMINS,
+                              callback_data='admin_list'))
+        bot.send_message(text=BOT_ACTION_SELECT,
+                         chat_id=call.message.chat.id,
+                         reply_markup=keyboard)
+    elif call.data == 'prefs_return':
+        home(call.message)
+
+
+@bot.callback_query_handler(func=Prefix('admin_'))
+def admin_callback(call):
+    if call.data == 'admin_add':
+        msg = bot.send_message(text=PREF_ADD_ADMIN_USR,
+                               chat_id=call.message.chat.id)
+        bot.register_next_step_handler(msg, add_admin)
+    elif call.data == 'admin_del':
+        msg = bot.send_message(text=PREF_DEL_ADMIN_USR,
+                               chat_id=call.message.chat.id)
+        bot.register_next_step_handler(msg, del_admin)
+    elif call.data == 'admin_list':
+        msg = PREF_CURR_ADMINS + ':\n'
+        db = psycopg2.connect(**db_creds)
+        c = db.cursor()
+        c.execute('''SELECT username FROM admins''')
+        for idx, user in enumerate(c.fetchall(), 1):
+            msg += '{}) @{}\n'.format(idx, user[0])
+        bot.send_message(text=msg,
+                         chat_id=call.message.chat.id)
+        home(call.message)
 
 
 if __name__ == '__main__':
